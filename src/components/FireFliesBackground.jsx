e8a4143963fdbc6cffed38b9e120bc987a165530
Johncross Moses-Madu <jmosesmadu32@gmail.com>
2024-08-21 06:00:04 +0100

    first commit

---
+++ src/components/FireFliesBackground.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const createFireFly = () => ({
+  id: Math.random,
+  top: `${Math.random() * 100}%`,
+  left: `${Math.random() * 100}%`,
+  animationDuration: `${Math.random() * 5 + 5}s`,
+});
+
+const FireFliesBackground = () => {
+  const [fireflies, setFireflies] = useState([]);
+
+  useEffect(() => {
+    const addFireflyPeriodically = () => {
+      const newFirefly = createFireFly();
+      setFireflies((prevFireFlies) => [
+        ...prevFireFlies.slice(-14),
+        newFirefly,
+      ]);
+    };
+
+    const interval = setInterval(addFireflyPeriodically, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex top-0 left-0 w-full h-full -z-10 overflow-hidden">
+      {fireflies.map((firefly) => {
+        return (
+          <div
+            key={firefly.id}
+            className="absolute rounded-full w-[10px] h-[10px] bg-firefly-radial"
+            style={{
+              top: firefly.top,
+              left: firefly.left,
+              animation: `move ${firefly.animationDuration} infinite alternate`,
+            }}
+          ></div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FireFliesBackground;
